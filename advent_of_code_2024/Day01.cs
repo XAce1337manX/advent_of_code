0eaeeff3bdeb736622dfe8133ccfc46607fe63a9
Bullet <68104413+XAce1337manX@users.noreply.github.com>
2024-12-01 15:45:01 +1000

    2024 Day 1

---
+++ advent_of_code_2024/Day01.cs
@@ -0,0 +1,35 @@
+﻿namespace advent_of_code_2024;
+
+public static class Day01
+{
+    public static void Solve(string inputFile)
+    {
+        var input = File.ReadAllLines(inputFile);
+        
+        var left = new List<int>();
+        var right = new List<int>();
+
+        foreach (var line in input)
+        {
+            var numbers = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            left.Add(int.Parse(numbers[0]));
+            right.Add(int.Parse(numbers[1]));
+        }
+
+        left.Sort();
+        right.Sort();
+
+        var partOneTotal = 0;
+        var partTwoTotal = 0;
+        for (var i = 0; i < left.Count; i++)
+        {
+            var number = left[i];
+            var similarity = right.Count(x => x == number);
+            partOneTotal += Math.Abs(left[i] - right[i]);
+            partTwoTotal += similarity * number;
+        }
+
+        Console.WriteLine(partOneTotal);
+        Console.WriteLine(partTwoTotal);
+    }
+}
---
+++ advent_of_code_2024/Program.cs
@@ -0,0 +1,4 @@
+﻿using advent_of_code_2024;
+
+Console.WriteLine("Day 1");
+Day01.Solve("input1.txt");
