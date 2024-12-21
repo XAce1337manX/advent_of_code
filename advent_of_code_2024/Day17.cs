c7610d06842bde80060395cac9a73c3e6c3b4ce3
Bullet <68104413+XAce1337manX@users.noreply.github.com>
2024-12-21 18:18:09 +1000

    2024 Day 17 Part 2

--- advent_of_code_2024/Day17.cs
+++ advent_of_code_2024/Day17.cs
@@ -7,12 +7,85 @@ public static class Day17
     public static void Solve(string inputFile)
     {
         var input = File.ReadAllLines(inputFile);
-        var registerA = int.Parse(input[0].Split(' ').Last());
-        var registerB = int.Parse(input[1].Split(' ').Last());
-        var registerC = int.Parse(input[2].Split(' ').Last());
-        var program = input[4].Split(' ').Last().Split(',').Select(int.Parse).ToList();
+        var registerA = long.Parse(input[0].Split(' ').Last());
+        var registerB = long.Parse(input[1].Split(' ').Last());
+        var registerC = long.Parse(input[2].Split(' ').Last());
+        var program = input[4].Split(' ').Last().Split(',').Select(long.Parse).ToList();
+
+        var partOneTotal = string.Join(',', GetOutput(program, registerA, registerB, registerC));
         
-        var output = new List<int>();
+        var changingBaseIndex = 0;
+        var currentStringResult = "";
+        var stringBuilder = new StringBuilder();
+        var invalidBaseIndexValues = new HashSet<(int @index, long value)>();
+        
+        while (changingBaseIndex < program.Count)
+        {
+            var proceedToNextBase = false;
+            
+            foreach (var i in Enumerable.Range(0, 8))
+            {
+                stringBuilder.Clear();
+                stringBuilder.Append(currentStringResult);
+                stringBuilder.Append(i);
+
+                while (stringBuilder.Length < program.Count)
+                {
+                    stringBuilder.Append('0');
+                }
+                
+                registerA = StringToBase8(stringBuilder.ToString());
+                var output = GetOutput(program, registerA, registerB, registerC);
+                if (output.Count != program.Count)
+                {
+                    continue;
+                }
+
+                if (output[^(changingBaseIndex + 1)] == program[^(changingBaseIndex + 1)]
+                    && !invalidBaseIndexValues.Contains((changingBaseIndex, i)))
+                {
+                    currentStringResult += i.ToString();
+                    proceedToNextBase = true;
+                    Console.WriteLine($"Search: {currentStringResult}");
+                    break;
+                }
+            }
+
+            if (proceedToNextBase)
+            {
+                invalidBaseIndexValues.RemoveWhere(x => x.index > changingBaseIndex);
+                changingBaseIndex++;
+            }
+            else
+            {
+                invalidBaseIndexValues.Add((index: changingBaseIndex - 1, value: long.Parse(currentStringResult[^1].ToString())));
+                currentStringResult = currentStringResult.Remove(currentStringResult.Length - 1);
+                changingBaseIndex--;
+            }
+        }
+
+        var partTwoTotal = StringToBase8(currentStringResult);
+        
+        Console.WriteLine($"Part One: {partOneTotal}");
+        Console.WriteLine($"Part Two: {partTwoTotal}");
+    }
+
+    private static long StringToBase8(string input)
+    {
+        long value = 0;
+        
+        var length = input.Length;
+        for (var i = 0; i < length; i++)
+        {
+            value += (long)Math.Pow(8, i) * long.Parse(input[^(i + 1)].ToString());
+        }
+
+        return value;
+    }
+
+    private static List<long> GetOutput(List<long> program, long registerA, long registerB, long registerC)
+    {
+        var output = new List<long>();
         
         var pointer = 0;
         while (pointer < program.Count - 1)
@@ -32,15 +105,16 @@ public static class Day17
                 6 => registerC,
                 _ => throw new Exception()
             };
-            
-            int numerator, denominator;
+
+            long numerator;
+            long denominator;
             
             switch (opcode)
             {
                 case 0:
                     numerator = registerA;
-                    denominator = (int)Math.Pow(2, comboOperand);
-                    registerA = numerator / denominator;
+                    denominator = (long)Math.Pow(2, comboOperand);
+                    registerA = (numerator / denominator);
                     break;
                 
                 case 1:
@@ -48,13 +122,13 @@ public static class Day17
                     break;
                 
                 case 2:
-                    registerB = comboOperand % 8;
+                    registerB = ((comboOperand % 8) + 8) % 8;
                     break;
                 
                 case 3:
                     if (registerA != 0)
                     {
-                        pointer = literalOperand;
+                        pointer = (int)literalOperand;
                         continue;
                     }
                     break;
@@ -83,7 +157,6 @@ public static class Day17
             pointer += 2;
         }
         
-        Console.WriteLine($"Part One: {string.Join(',', output)}");
-        // Console.WriteLine($"Part Two: {partTwoTotal}");
+        return output;
     }
 }
