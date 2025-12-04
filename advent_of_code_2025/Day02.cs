b438d5aaa8d29601fd99df5e6d339ab35f009073
Bullet <68104413+XAce1337manX@users.noreply.github.com>
2025-12-04 23:55:58 +1000

    2025 Day 2 Part 1

---
+++ advent_of_code_2025/Day02.cs
@@ -0,0 +1,44 @@
+﻿namespace advent_of_code_2025;
+
+public static class Day02
+{
+    public static void Solve(string inputFile)
+    {
+        var input = File.ReadAllLines(inputFile);
+
+        var ranges = input[0].Split(',');
+
+        var partOneTotal = 0L;
+        foreach (var range in ranges)
+        {
+            var idRanges = range.Split('-');
+            
+            var start = long.Parse(idRanges[0]);
+            var end = long.Parse(idRanges[1]);
+
+            for (var id = start; id <= end; id++)
+            {
+                var digits = (long)Math.Round(Math.Floor(Math.Log10(id) + 1));
+
+                // Odd is automatically valid
+                if (digits % 2 == 1)
+                {
+                    continue;
+                }
+                
+                var splitter = Math.Pow(10, digits / 2);
+                
+                var leftSide = (long)(id / splitter);
+                var rightSide = (long)(id % splitter);
+
+                if (leftSide == rightSide)
+                {
+                    partOneTotal += id;
+                    Console.WriteLine($"Invalid {id} in range {start} to {end}");
+                }
+            }
+        }
+        
+        Console.WriteLine($"Day 2 - Part 1: {partOneTotal}");
+    }
+}
