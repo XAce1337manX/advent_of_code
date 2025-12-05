78414a7a88dac1f607e51f279a4272284f6724b3
Bullet <68104413+XAce1337manX@users.noreply.github.com>
2025-12-05 22:09:26 +1000

    2025 Day 3 Part 1

---
+++ advent_of_code_2025/Day03.cs
@@ -0,0 +1,43 @@
+﻿namespace advent_of_code_2025;
+
+public static class Day03
+{
+    public static void Solve(string inputFile)
+    {
+        var input = File.ReadAllLines(inputFile);
+
+        var partOneTotal = 0;
+        
+        foreach (var bank in input)
+        {
+            var leftMax = '0';
+            var rightMax = '0';
+            for (var i = 0; i < bank.Length - 1; i++)
+            {
+                var leftBattery = bank[i];
+                if (leftBattery <= leftMax)
+                {            
+                    continue;
+                }
+                
+                leftMax = leftBattery;
+
+                rightMax = '0';
+                for (var j = i + 1; j < bank.Length; j++)
+                {
+                    var rightBattery = bank[j];
+                    if (rightMax < rightBattery)
+                    {
+                        rightMax = rightBattery;
+                    }
+                }
+            }
+
+            Console.WriteLine($"J {leftMax}-{rightMax}");
+            var joltage = int.Parse($"{leftMax}{rightMax}");
+            partOneTotal += joltage;
+        }
+        
+        Console.WriteLine($"Day 2 - Part 1: {partOneTotal}");
+    }
+}
